54567ad760f0e2a6cb3a536046dbc08353fdfa5a
sandipperf <sandip.perf@gmail.com>
2019-11-28 13:03:18 +0000

    Update and rename get_data_using_nsepy.py to analyse_stock_eq.py

--- analyse_stock_eq.py
+++ analyse_stock_eq.py
@@ -1,8 +1,10 @@
 from datetime import date
+
+import time
 import pandas as pd
 import sys
-import time
 from nsepy import get_history
+import numpy as np
 
 
 #input definitions
@@ -17,6 +19,7 @@ input_fut_exp_date2 = sys.argv[6]
 equity_result_FileName = f'{outputDir}/cash_market_details_{input_stock_name}.csv'
 fut_result_FileName1 = f'{outputDir}/fut_market_details_currentmonth_{input_stock_name}.csv'
 fut_result_FileName2 = f'{outputDir}/fut_market_details_nextmonth_{input_stock_name}.csv'
+final_result_file = f'{outputDir}/Result_{input_stock_name}.csv'
 
 #preparing inputs from given input
 fields_start_eq = input_start_date.split(",")
@@ -30,16 +33,15 @@ input_eq_end_month=int(fields_end_eq[1])
 input_eq_end_day=int(fields_end_eq[2])
 
 #input_fut_exp_date1
-fileds_fut_exp1 = input_fut_exp_date1.split(",")
-input_fut_exp1_year=int(fileds_fut_exp1[0])
-input_fut_exp1_month=int(fileds_fut_exp1[1])
-input_fut_exp1_day=int(fileds_fut_exp1[2])
+fields_fut_exp1 = input_fut_exp_date1.split(",")
+input_fut_exp1_year=int(fields_fut_exp1[0])
+input_fut_exp1_month=int(fields_fut_exp1[1])
+input_fut_exp1_day=int(fields_fut_exp1[2])
 
-fileds_fut_exp2 = input_fut_exp_date2.split(",")
-input_fut_exp2_year=int(fileds_fut_exp2[0])
-input_fut_exp2_month=int(fileds_fut_exp2[1])
-input_fut_exp2_day=int(fileds_fut_exp2[2])
-#print(input_start_date,input_end_date,input_fut_exp_date1,input_fut_exp_date2)
+fields_fut_exp2 = input_fut_exp_date2.split(",")
+input_fut_exp2_year=int(fields_fut_exp2[0])
+input_fut_exp2_month=int(fields_fut_exp2[1])
+input_fut_exp2_day=int(fields_fut_exp2[2])
 
 #getting Equity details from nse and write to a file
 def get_equity_data_from_nse():
@@ -77,18 +79,50 @@ def get_futures__nextmonthdetails_from_nse():
     fut_query_result2.close();
 
 def read_cash_dat():
-    cash_d = pd.read_csv(equity_result_FileName)
-    #cash_d['YClose'] = (pd.to_numeric(cash_d['Prev Close']))
-    #print(cash_d['YClose'])
-    #print(cash_d.head());
-    for col in cash_d.columns:
-        print(col)
-        exit();
+
+    eq_data = pd.read_csv(equity_result_FileName, encoding='ISO-8859\xe2\x80\x931')
+    fut_current_month = pd.read_csv(fut_result_FileName1, encoding='ISO-8859\xe2\x80\x931')
+    fut_next_month = pd.read_csv(fut_result_FileName2, encoding='ISO-8859\xe2\x80\x931')
+
+    result_fut_merge_df = pd.merge(fut_current_month, fut_next_month, on='Date', how='left')
+
+    result_fut_merge_df['cumulation_oi'] = result_fut_merge_df['Open Interest_x'] + result_fut_merge_df['Open Interest_y']
+
+    result_fut_merge_df['cumulation_change_oi'] = result_fut_merge_df['Change in OI_x'] + result_fut_merge_df['Change in OI_y']
+    result_fut_merge_df['cumulation_contract'] = result_fut_merge_df['Number of Contracts_x'] + result_fut_merge_df['Number of Contracts_y']
+    result_fut_merge_df['Change_in_OI_DivBy_Contract'] = round(result_fut_merge_df['cumulation_oi'] / result_fut_merge_df['cumulation_contract'], 2)
 
 
 
-get_equity_data_from_nse();
-time.sleep(320);
-get_futures__currentmonthdetails_from_nse();
-time.sleep(320);
-get_futures__nextmonthdetails_from_nse();
+    final_result_df = pd.DataFrame();
+    final_result_df['date_eq'] = eq_data['Date'];
+    final_result_df['Cash_LTP'] = eq_data['Last'];
+    final_result_df['VWAP'] = eq_data['VWAP'];
+    final_result_df['Cash_Volume_Divby_Trade'] = round(eq_data['Volume'] / eq_data['Trades'], 2);
+    final_result_df['%Deliverble'] = round(eq_data['%Deliverble'] * 100, 2);
+    final_result_df['Cummulative_OI'] = result_fut_merge_df['cumulation_oi']
+    final_result_df['Cummulative_Change_in_OI'] = result_fut_merge_df['cumulation_change_oi'];
+
+    final_result_df['Change_in_OI_DivBy_Contract'] = result_fut_merge_df['Change_in_OI_DivBy_Contract'];
+    final_result_df['Open'] = eq_data['Open'];
+    final_result_df['High'] = eq_data['High'];
+    final_result_df['Low'] = eq_data['Low'];
+    final_result_df['YClose'] = eq_data['Prev Close'];
+    final_result_df['%Change'] = round(((final_result_df['Cash_LTP'] - final_result_df['YClose'])/final_result_df['YClose'])*100, 2);
+
+
+    #Print result to csv file
+    final_result_df.to_csv(final_result_file, index=False)
+
+
+
+
+
+
+
+#get_equity_data_from_nse();
+#time.sleep(320);
+#get_futures__currentmonthdetails_from_nse();
+#time.sleep(320);
+#get_futures__nextmonthdetails_from_nse();
+read_cash_dat();
