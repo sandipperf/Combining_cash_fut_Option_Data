fde4d5f59c189aa0acb275c1c2858a68a294b989
sandipperf <sandip.perf@gmail.com>
2019-11-28 13:03:55 +0000

    Update master.sh

--- master.sh
+++ master.sh
@@ -1,9 +1,10 @@
-input_stock_name="TATAMOTORS"
-outputDir="TATA_analysis"
+
+input_stock_name="INFY"
+outputDir="INFY_Final"
 input_start_date="2019,11,3"
-input_end_date="2019,11,22"
+input_end_date="2019,11,26"
 input_fut_exp_date1="2019,11,28"
 input_fut_exp_date2="2019,12,26"
 
 mkdir -p $outputDir
-python3 get_stock_eq.py $input_stock_name $outputDir $input_start_date $input_end_date $input_fut_exp_date1 $input_fut_exp_date2
+python3 analyse_stock_eq.py $input_stock_name $outputDir $input_start_date $input_end_date $input_fut_exp_date1 $input_fut_exp_date2
